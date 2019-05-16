33e16c2fe08d5e9be23a6f1fb908ba630bd8fe19
Mac Strelioff <mstrelio@uci.edu>
2019-05-15 17:00:11 -0700

    auto

--- MacStrelioff_Source/content/Unlisted/Example/app.R
+++ MacStrelioff_Source/content/Unlisted/Example/app.R
@@ -13,17 +13,20 @@
 # rsconnect::deployApp("<PATH TO APP>")
 # rsconnect::deployApp("/Users/mac/git/macstrelioff.github.io/MacStrelioff_Source/content/Unlisted/Example")
 
+# for a rock-paper-scissors bot, can enumerate 5-gram outcomes with; 
+# all_5_grams <- expand.grid(p1 = c(1,2,3), p2 = c(1,2,3), p3 = c(1,2,3),p4 = c(1,2,3),p5 = c(1,2,3), stringsAsFactors = FALSE) 
+
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Rescorla Wagner Model Example"),
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
          sliderInput("learning_rate",
-                     "Number of bins:",
+                     "Learning Rate:",
                      min = 0,
                      max = 1,
                      value = .2,
@@ -38,27 +41,37 @@ ui <- fluidPage(
    )
 )
 
-
-# initialize variables (runs once when app visited)
-t=0;     # timestep
-v=0;     # value
-vs = v   # values to track
-
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  # initialize variables (runs once when app visited)
+  values <- reactiveValues()
+  values$t=0;     # timestep
+  values$v=0;     # value
+  values$vs = 0   # values to track
+  values$rs = 0
+  
+  # text feedback
+  observeEvent(input$step,{
+    # Create dependency on the button 'step'
+    input$step
+    values$t = values$t+1 # step in time
+    r = rbinom(size=1,n=1,.7)
+    pe= r-values$v # prediction error
+    values$v = values$v+isolate(input$learning_rate) * pe
+    values$vs= c(values$vs,values$v)
+    values$rs= c(values$rs,r)
+    cat("\n event observed:","t=",values$t,sep="")
+  })
+  
   output$distPlot <- renderPlot({
-     # Create dependency on the button 'step'
-     input$step
-     t = t+1 # step in time
-     r = rbinom(size=1,n=1,.8)
-     pe= r-v # prediction error
-     v = v+isolate(input$learning_rate) * pe
-     vs= c(vs,v)
      # generate bins based on input$bins from ui.R
-     x = seq(0,t)
+     x = seq(0,values$t)
      # draw the histogram with the specified number of bins
-     plot(x,vs,type="l",main=paste(v))
-     points(x,vs)
+     plot(x,values$vs,type="l",xlab = "Trials",ylab="Value Estimate",
+          ylim=c(0,1))
+     points(x,values$vs,pch=values$rs*15+4,col=values$rs+2)
+     abline(h=.7,lty=2)
    })
 }
 
